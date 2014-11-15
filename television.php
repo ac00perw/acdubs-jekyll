836d221bf8e214b730c40c791bf3fcc31c26699a
Adam-MBP <a@acdubs.com>
2014-11-14 20:07:53 -0500

    tv stuff

--- television.php
+++ television.php
@@ -1,10 +1,13 @@
 ---
-layout: empty
-permalink: /television/index.php
+layout: default
+permalink: /tv/index.php
 ---
 <?php $f=json_decode(file_get_contents("http://noisebelt.com/television?type=soon")); ?>
+<p>The following is a list of television shows I'm currently watching. Some of them might be embarassing.</p>
 
-
-<?php foreach($f->data->soon as $k){ ?>
-	{% include tvepisode.html %}
-<?php } ?>
+<ul class="no-bullet">
+	<?php 
+	foreach($f->data->soon as $k){ ?>
+		<li class="tvshow" style="width: 50%; float: right">{% include tvepisode.html %} </li>
+	<?php	} ?>
+</ul>
